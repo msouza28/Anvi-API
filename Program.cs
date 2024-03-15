fafb8f17a9672391fe9301a723a37bfe9b00f190
msouza28 <matheus.emiliano28@gmail.com>
2024-03-15 12:17:24 -0300

    Refatoração para o padrao de repositorios

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Anvi_API.Data;
+using Anvi_API.Repository.Impl;
+using Anvi_API.Repository.Interface;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(op => {
     op.UseSqlite("DataSource=anvi.db");
 });
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
---
+++ Repository/Interface/IUsuarioRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anvi_API.Dtos.usuario;
+using Anvi_API.Models;
+
+namespace Anvi_API.Repository.Interface
+{
+    public interface IUsuarioRepository
+    {
+        public Task<List<Usuario>> GetAllUsuariosAsync();
+
+        public Task<Usuario?> GetUsuarioByIdAsync(long id);
+
+        public Task<Usuario> CreateUsuarioAsync(Usuario usuarioModel);
+
+        public Task<Usuario?> UpdateUsuarioAsync(long id, UpdateUsuarioRequestDto requestDto);
+
+        public Task<Usuario?> DeleteUsuario(long id);
+    }
+}
